05821e90f4b43ee06322b41e4a5a4c9613f1a7cf
dmitry <strukovsky1@gmail.com>
2021-10-30 09:56:08 +0300

    Add entities to AppDatabase signature

--- app/src/main/java/com/strukovsky/financiel/db/AppDatabase.kt
+++ app/src/main/java/com/strukovsky/financiel/db/AppDatabase.kt
@@ -5,10 +5,11 @@ import androidx.room.RoomDatabase
 import com.strukovsky.financiel.db.dao.BalanceSheetDao
 import com.strukovsky.financiel.db.dao.CashFlowDao
 import com.strukovsky.financiel.db.dao.ShareDao
+import com.strukovsky.financiel.db.entity.BalanceSheet
 import com.strukovsky.financiel.db.entity.CashFlow
 import com.strukovsky.financiel.db.entity.Share
 
-@Database(entities = [Share::class], version = 1, exportSchema = false)
+@Database(entities = [Share::class, CashFlow::class, BalanceSheet::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
    abstract val shareDao: ShareDao
    abstract val cashFlowDao: CashFlowDao
