fa24bb43e4fad1c77fd3e9c25d2a973993a4fbea
dmitry <strukovsky1@gmail.com>
2021-11-11 11:57:23 +0300

    Fix bug with ShareAdapter.kt

--- app/src/main/java/com/strukovsky/financiel/ui/adapter/ShareAdapter.kt
+++ app/src/main/java/com/strukovsky/financiel/ui/adapter/ShareAdapter.kt
@@ -1,7 +1,6 @@
 package com.strukovsky.financiel.ui.adapter
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,9 +19,9 @@ class ShareAdapter(private var data: List<Share>): RecyclerView.Adapter<ShareAda
         val shareName: TextView = view.findViewById(R.id.share_name)
     }
 
-    fun updateData(shares: Any)
+    fun updateData(shares: List<*>)
     {
-        data = shares as List<Share>
+        data = shares.map { it as Share }
         this.notifyDataSetChanged()
     }
 
